b0d0cbc8f6eaa90342520c50f224ee6fafff131f
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-04-21 21:47:46 +0300

    Create batch normalization 2D layer

---
+++ Source/Atum/Private/Layers/AtumLayerBatchNorm2D.cpp
@@ -0,0 +1,3 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "Layers/AtumLayerBatchNorm2D.h"
---
+++ Source/Atum/Public/Layers/AtumLayerBatchNorm2D.h
@@ -0,0 +1,14 @@
+﻿// © 2023 Kaya Adrian.
+
+#pragma once
+
+#include "IAtumLayer.h"
+
+#include "AtumLayerBatchNorm2D.generated.h"
+
+
+UCLASS(Blueprintable, BlueprintType, DisplayName = "ATUM Batch Norm 2D Layer")
+class ATUM_API UAtumLayerBatchNorm2D : public UObject, public IAtumLayer, public TAtumLayer<torch::nn::BatchNorm2dImpl>
+{
+	GENERATED_BODY()
+};
