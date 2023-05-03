d7494c6bed0386a4e429df42a126da43546a97cc
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-05-04 00:28:27 +0300

    Update error log messages

--- Source/Atum/Private/Layers/IAtumLayer.cpp
+++ Source/Atum/Private/Layers/IAtumLayer.cpp
@@ -20,17 +20,17 @@ bool IAtumLayer::Forward_Implementation(
 ) noexcept
 {
 	UObject* const LayerObject = _getUObject();
-	const TCHAR* const LayerClassName = *LayerObject->GetClass()->GetName();
+	const TCHAR* const LayerName = *GetNameSafe(LayerObject);
 
 	if (const torch::Tensor* const Data = Input ? Input->GetData() : nullptr; Data == nullptr)
 	{
-		UE_LOG(LogAtum, Error, TEXT("Input had no data at ATUM Layer of type %s!"), LayerClassName)
+		UE_LOG(LogAtum, Error, TEXT("Input had no data at ATUM Layer of type `%s`!"), LayerName)
 		return false;
 	}
 	
 	if (!Execute_InitializeData(LayerObject, false))
 	{
-		UE_LOG(LogAtum, Error, TEXT("Could not initialize ATUM Layer of type %s!"), LayerClassName)
+		UE_LOG(LogAtum, Error, TEXT("Could not initialize ATUM Layer of type `%s`!"), LayerName)
 		return false;
 	}
 
@@ -39,17 +39,25 @@ bool IAtumLayer::Forward_Implementation(
 	{
 		bSuccess = Execute_OnForward(LayerObject, Input, Output);
 	}
-	catch (const c10::Error& Error)
+	catch (const std::exception& Exception)
 	{
-		UE_LOG(LogAtum, Error, TEXT("Unhandled exception in %s - %hs"), LayerClassName, Error.what_without_backtrace())
 		bSuccess = false;
+
+		const std::string& ExceptionString = Exception.what();
+		UE_LOG(
+			LogAtum,
+			Error,
+			TEXT("Unhandled exception in `%s` layer - %hs"),
+			LayerName,
+			ExceptionString.substr(0, ExceptionString.find("\n")).c_str()
+		)
 	}
 	return bSuccess;
 }
 
 bool IAtumLayer::OnInitializeData_Implementation(const bool bRetry) noexcept
 {
-	UE_LOG(LogAtum, Error, TEXT("OnInitializeData is not implemented on %s!"), *_getUObject()->GetClass()->GetName())
+	UE_LOG(LogAtum, Error, TEXT("OnInitializeData is not implemented in `%s`!"), *_getUObject()->GetClass()->GetName())
 	return false;
 }
 
@@ -58,7 +66,7 @@ bool IAtumLayer::OnForward_Implementation(
 	TScriptInterface<IAtumTensor>& Output
 )
 {
-	UE_LOG(LogAtum, Error, TEXT("OnForward is not implemented on %s!"), *_getUObject()->GetClass()->GetName())
+	UE_LOG(LogAtum, Error, TEXT("OnForward is not implemented in `%s`!"), *_getUObject()->GetClass()->GetName())
 	
 	Output = DuplicateObject(Input.GetObject(), nullptr);
 	return false;
