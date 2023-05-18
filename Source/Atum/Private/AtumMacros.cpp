c147accb3d268186d64341be4076a78c65127c16
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-05-18 19:34:15 +0300

    Store macros in one file

---
+++ Source/Atum/Private/AtumMacros.cpp
@@ -0,0 +1,6 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "AtumMacros.h"
+
+
+//DEFINE_LOG_CATEGORY(LogAtum)
---
+++ Source/Atum/Public/AtumMacros.h
@@ -0,0 +1,28 @@
+﻿// © 2023 Kaya Adrian.
+
+#pragma once
+
+#include "Logging/LogMacros.h"
+
+
+//DECLARE_LOG_CATEGORY_EXTERN(LogAtum, Log, All)
+
+#define ATUM_LOG(Verbosity, Format, ...) UE_LOG(LogAtum, Verbosity, Format, ##__VA_ARGS__)
+
+/*
+#define GENERATED_ATUM_LAYER(ModuleClass) \
+protected: \
+	ModuleClass Module = nullptr; \
+	\
+public: \
+	UE_NODISCARD \
+	virtual std::shared_ptr<torch::nn::Module> GetSharedModule() const noexcept override { return Module.ptr(); } \
+	\
+	UE_NODISCARD \
+	FORCEINLINE const ModuleClass& GetModule() const noexcept { return Module; } \
+	\
+	UE_NODISCARD \
+	FORCEINLINE ModuleClass& GetModule() noexcept { return Module; } \
+	\
+private:
+*/
