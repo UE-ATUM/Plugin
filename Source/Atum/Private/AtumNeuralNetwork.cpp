94d73667b5309146285e7c6383795246bd6f695e
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-05-18 19:34:26 +0300

    Implement rudimentary neural network

--- Source/Atum/Private/AtumNeuralNetwork.cpp
+++ Source/Atum/Private/AtumNeuralNetwork.cpp
@@ -1,3 +1,78 @@
 ﻿// © 2023 Kaya Adrian.
 
 #include "AtumNeuralNetwork.h"
+
+#include "AtumMacros.h"
+#include "IAtum.h"
+
+
+// ReSharper disable CppUE4CodingStandardNamingViolationWarning
+namespace torch::nn
+{
+	void AtumNetworkImpl::reset()
+	{
+	}
+
+	void AtumNetworkImpl::pretty_print(std::ostream& stream) const
+	{
+		stream << "torch::nn::AtumNetwork";
+	}
+}
+// ReSharper restore CppUE4CodingStandardNamingViolationWarning
+
+UAtumNeuralNetwork::UAtumNeuralNetwork() noexcept
+{
+	NamedLayers.Add(TEXT("Network Input"), this);
+}
+
+void UAtumNeuralNetwork::RegisterLayer(const FName Name, const TScriptInterface<IAtumLayer>& Layer) noexcept
+{
+	const ANSICHAR* const NetworkName = TCHAR_TO_UTF8(*GetNameSafe(this));
+	if (UNLIKELY(!Execute_InitializeData(this, false)))
+	{
+		ATUM_LOG(Error, TEXT("Could not initialize ATUM Neural Network instance called `%hs`!"), NetworkName)
+		return;
+	}
+	
+	if (Layer == nullptr || !Layer->IsInitialized())
+	{
+		ATUM_LOG(Error, TEXT("Could not register uninitialized layer in `%hs` ATUM Neural Network!"), NetworkName)
+		return;
+	}
+	
+	NamedLayers.Add(Name, Layer);
+
+	for (const TTuple<FName, TScriptInterface<IAtumLayer>>& NamedLayer : NamedLayers)
+	{
+		std::ostringstream Stream;
+		NamedLayer.Value->GetSharedModule()->pretty_print(Stream);
+		UE_LOG(LogTemp, Warning, TEXT("%hs"), Stream.str().c_str())
+	}
+	UE_LOG(LogTemp, Warning, TEXT("DONE!\n\n"))
+}
+
+bool UAtumNeuralNetwork::OnInitializeData_Implementation([[maybe_unused]] const bool bRetry) noexcept
+{
+	Module.Reset(new torch::nn::AtumNetwork(std::make_shared<torch::nn::AtumNetworkImpl>()));
+	return true;
+}
+
+bool UAtumNeuralNetwork::OnForward_Implementation(
+	const TScriptInterface<IAtumTensor>& Input,
+	TScriptInterface<IAtumTensor>& Output
+)
+{
+	TScriptInterface<IAtumTensor> Subinput = Input;
+	for (const TTuple<FName, TScriptInterface<IAtumLayer>>& NamedLayer : NamedLayers)
+	{
+		const TScriptInterface<IAtumLayer>& Layer = NamedLayer.Value;
+		if (Layer == this)
+			continue;
+		
+		if (!Execute_Forward(Layer ? Layer->_getUObject() : nullptr, Subinput, Output))
+			return false;
+		
+		Subinput = Output;
+	}
+	return true;
+}
--- Source/Atum/Public/AtumNeuralNetwork.h
+++ Source/Atum/Public/AtumNeuralNetwork.h
@@ -4,11 +4,60 @@
 
 #include "Layers/IAtumLayer.h"
 
+LIBTORCH_INCLUDES_START
+#include "torch/nn/cloneable.h"
+LIBTORCH_INCLUDES_END
+
 #include "AtumNeuralNetwork.generated.h"
+
+
+// ReSharper disable CppUE4CodingStandardNamingViolationWarning
+namespace torch::nn
+{
+	class AtumNetworkImpl : public Cloneable<AtumNetworkImpl>
+	{
+	public:
+		UE_NODISCARD
+		// ReSharper disable once CppMemberFunctionMayBeStatic
+		FORCEINLINE Tensor forward(const Tensor& Input) { return Input; }
+
+		virtual void reset() override;
+		virtual void pretty_print(std::ostream& stream) const override;
+	};
+	TORCH_MODULE(AtumNetwork);
+}
+// ReSharper restore CppUE4CodingStandardNamingViolationWarning
 
 
 UCLASS(Blueprintable, BlueprintType, DisplayName = "ATUM Neural Network")
 class ATUM_API UAtumNeuralNetwork : public UObject, public IAtumLayer
 {
 	GENERATED_BODY()
+	GENERATED_ATUM_LAYER(torch::nn::AtumNetwork)
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ATUM|Network", meta = (AllowPrivateAccess))
+	TMap<FName, TScriptInterface<IAtumLayer>> NamedLayers;
+	
+public:
+	UE_NODISCARD_CTOR
+	UAtumNeuralNetwork() noexcept;
+	
+	UFUNCTION(BlueprintCallable)
+	void RegisterLayer(FName Name, const TScriptInterface<IAtumLayer>& Layer) noexcept;
+	
+protected:
+	virtual bool OnInitializeData_Implementation(bool bRetry = false) noexcept override;
+	
+	virtual bool OnForward_Implementation(
+		const TScriptInterface<IAtumTensor>& Input,
+		TScriptInterface<IAtumTensor>& Output
+	) override;
+	
+public:
+	UE_NODISCARD
+	FORCEINLINE const TMap<FName, TScriptInterface<IAtumLayer>>& GetNamedLayers() const noexcept { return NamedLayers; }
+	
+	UE_NODISCARD
+	FORCEINLINE TMap<FName, TScriptInterface<IAtumLayer>>& GetNamedLayers() noexcept { return NamedLayers; }
 };
