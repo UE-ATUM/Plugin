a4f04d1b6e4a79995e660e23923bfd42877025c9
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-06-02 07:03:11 +0300

    Create reinforcement learning environment base actor class

---
+++ Source/Atum/Private/Reinforcement/AtumEnvironment.cpp
@@ -0,0 +1,22 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "Reinforcement/AtumEnvironment.h"
+
+#include "Reinforcement/IAtumEnvironmentStateFinder.h"
+
+
+#define LOCTEXT_NAMESPACE "AtumEnvironment"
+
+UObject* AAtumEnvironment::ResetState() const
+{
+	OnEnvironmentResetState.Broadcast(this);
+	return GetState();
+}
+
+UObject* AAtumEnvironment::GetState() const
+{
+	UObject* const StateObject = StateFinder.GetObject();
+	return StateObject && IAtumEnvironmentStateFinder::Execute_FindStateIn(StateObject, this) ? StateObject : nullptr;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Atum/Private/Reinforcement/IAtumEnvironmentStateFinder.cpp
@@ -0,0 +1,7 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "Reinforcement/IAtumEnvironmentStateFinder.h"
+
+
+#define LOCTEXT_NAMESPACE "IAtumEnvironmentStateFinder"
+#undef LOCTEXT_NAMESPACE
