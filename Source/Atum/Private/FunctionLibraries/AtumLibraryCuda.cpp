2fdaf0c24a1459849e55e4a21365692f9d7480dc
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-06-13 23:47:05 +0300

    Create Blueprint function library related to CUDA

---
+++ Source/Atum/Private/FunctionLibraries/AtumLibraryCuda.cpp
@@ -0,0 +1,7 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "FunctionLibraries/AtumLibraryCuda.h"
+
+
+#define LOCTEXT_NAMESPACE "AtumLibraryCuda"
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Atum/Public/FunctionLibraries/AtumLibraryCuda.h
@@ -0,0 +1,32 @@
+﻿// © 2023 Kaya Adrian.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Macros/AtumMacrosGuards.h"
+
+TORCH_INCLUDES_START
+#include <torch/cuda.h>
+TORCH_INCLUDES_END
+
+#include "AtumLibraryCuda.generated.h"
+
+
+#define LOCTEXT_NAMESPACE "AtumLibraryCuda"
+
+UCLASS(Abstract, Blueprintable, BlueprintType, DisplayName = "ATUM CUDA Library")
+class ATUM_API UAtumLibraryCuda : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UE_NODISCARD
+	UFUNCTION(BlueprintPure, Category = "ATUM|CUDA")
+	static FORCEINLINE bool IsCudaAvailable() noexcept { return torch::cuda::is_available(); }
+	
+	UE_NODISCARD
+	UFUNCTION(BlueprintPure, Category = "ATUM|CUDA")
+	static FORCEINLINE bool IsCudnnAvailable() noexcept { return torch::cuda::cudnn_is_available(); }
+};
+
+#undef LOCTEXT_NAMESPACE
