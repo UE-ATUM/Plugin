e9f1aa68319fa08e1f1e9472b469b1e84645d22b
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2023-08-25 19:21:41 +0300

    Add basic LSTM layer

---
+++ Source/Atum/Private/Layers/Recurrent/AtumLayerLstm.cpp
@@ -0,0 +1,52 @@
+﻿// © 2023 Kaya Adrian.
+
+#include "Layers/Recurrent/AtumLayerLstm.h"
+
+#include "Tensors/IAtumTensor.h"
+
+
+#define LOCTEXT_NAMESPACE "AtumLayerLstm"
+
+UAtumLayerLstm::UAtumLayerLstm() noexcept : HiddenLayer(nullptr)
+{
+}
+
+bool UAtumLayerLstm::OnInitializeData_Implementation(const bool bRetry)
+{
+	if (bRetry)
+	{
+		HiddenLayer = nullptr;
+	}
+	
+	Module = MakeShared<torch::nn::LSTM>(std::make_shared<torch::nn::LSTMImpl>(
+		static_cast<torch::nn::LSTMOptions>(Options)
+	));
+	return true;
+}
+
+bool UAtumLayerLstm::OnForward_Implementation(
+	const TScriptInterface<IAtumTensor>& Input,
+	TScriptInterface<IAtumTensor>& Output
+)
+{
+	std::tuple<at::Tensor, std::tuple<at::Tensor, at::Tensor>> LstmOutputs = (*Module)->forward(
+		Input->GetDataChecked(),
+		HiddenLayer ? *HiddenLayer : std::tuple<at::Tensor, at::Tensor>()
+	);
+	HiddenLayer = TSharedPtr<std::tuple<at::Tensor, at::Tensor>>(&std::get<1ULL>(LstmOutputs));
+	
+	Output = DuplicateObject(Input.GetObject(), nullptr);
+	Output->SetData(std::get<0ULL>(LstmOutputs).squeeze(0LL));
+	return true;
+}
+
+bool UAtumLayerLstm::LoadFromFile_Implementation(const FString& RelativePath)
+{
+	if (!IAtumLayer::LoadFromFile_Implementation(RelativePath))
+		return false;
+	
+	Options.SetFrom((*Module)->options);
+	return true;
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Atum/Public/Layers/Recurrent/AtumLayerLstmOptions.h
@@ -0,0 +1,64 @@
+﻿// © 2023 Kaya Adrian.
+
+#pragma once
+
+#include "Layers/AtumLayerBaseOptions.h"
+#include "Macros/AtumMacrosGuards.h"
+
+TORCH_INCLUDES_START
+#include <torch/nn/options/rnn.h>
+TORCH_INCLUDES_END
+
+#include "AtumLayerLstmOptions.generated.h"
+
+
+#define LOCTEXT_NAMESPACE "AtumLayerLstmOptions"
+
+USTRUCT(BlueprintType, DisplayName = "ATUM LSTM Layer Options")
+struct ATUM_API FAtumLayerLstmOptions : public FAtumLayerBaseOptions
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	int64 InputSize;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	int64 HiddenSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	int64 LayerCount;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bBias;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bBatchFirst;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	double Dropout;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	bool bBidirectional;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess))
+	int64 ProjectionSize;
+	
+	UE_NODISCARD_CTOR
+	FAtumLayerLstmOptions() noexcept;
+	
+	UE_NODISCARD
+	FORCEINLINE explicit operator torch::nn::LSTMOptions() const noexcept
+	{
+		return torch::nn::LSTMOptions(InputSize, HiddenSize)
+		.num_layers(LayerCount)
+		.bias(bBias)
+		.batch_first(bBatchFirst)
+		.dropout(Dropout)
+		.bidirectional(bBidirectional)
+		.proj_size(ProjectionSize);
+	}
+	
+	void SetFrom(const torch::nn::LSTMOptions& Options) noexcept;
+};
+
+#undef LOCTEXT_NAMESPACE
